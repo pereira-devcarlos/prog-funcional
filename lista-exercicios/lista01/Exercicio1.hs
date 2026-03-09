f332072e3fca64a4eca570f6a923e5ebf650b9c4
Carlos Eduardo <contato.devcarlos@gmail.com>
2026-03-09 13:22:20 -0300

    altera a implementação das funções f1 e f2 e atualiza a função main para refletir as mudanças

--- lista-exercicios/lista01/Exercicio1.hs
+++ lista-exercicios/lista01/Exercicio1.hs
@@ -2,12 +2,20 @@
 ---Exercicio.hs
 --------------------------------------
 
-a :: Double -> Double
-a x
+f1 :: Double -> Double
+f1 x
     | x >=0 = x+4/x+2
     | x < 0 = 2/x
 
+f2 :: (Double, Double) -> Double
+f2 (x,y)
+    | x >= y = x + y
+    | x < y = x - y
+
 main :: IO()
 main = do 
-    print(a 2)
-    print(a (-2))
+    print(f1 2)
+    print(f1 (-2))
+    
+    print(f2 (4, 2))
+    print(f2 (-3, 3))
