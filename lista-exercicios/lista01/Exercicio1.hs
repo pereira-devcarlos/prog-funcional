9801d47af94e646e3917a6181f5df7b805ca66e9
Carlos Eduardo <contato.devcarlos@gmail.com>
2026-03-09 13:27:41 -0300

    adiciona a função f3 e atualiza a função main para incluir novos testes

--- lista-exercicios/lista01/Exercicio1.hs
+++ lista-exercicios/lista01/Exercicio1.hs
@@ -12,10 +12,25 @@ f2 (x,y)
     | x >= y = x + y
     | x < y = x - y
 
+f3 :: (Double, Double, Double) -> Double
+f3 (x,y,z)
+    | (x+y) > z = x + y + z
+    | (x+y) < z = x - y - z
+    | (x+y) == z = 0
+
 main :: IO()
 main = do 
+    putStrLn "Função 1"
     print(f1 2)
     print(f1 (-2))
-    
+    putStrLn ""
+
+    putStrLn "Função 2"
     print(f2 (4, 2))
     print(f2 (-3, 3))
+    putStrLn ""
+
+    putStrLn "Função 3"
+    print(f3 (2, 2, 3))
+    print(f3 (1, 2, 5))
+    print(f3 (2, 2, 4))
