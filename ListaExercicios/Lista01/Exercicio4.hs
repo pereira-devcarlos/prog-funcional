b25507ae22a4358e8f9b3b1cabf7f48ad99535a3
Carlos Eduardo <contato.devcarlos@gmail.com>
2026-03-11 13:27:58 -0300

    remove funções auxiliares div10 e mod10, ajusta a função aux e atualiza a função main para exibir o resultado de invertInt

--- ListaExercicios/Lista01/Exercicio4.hs
+++ ListaExercicios/Lista01/Exercicio4.hs
@@ -2,23 +2,12 @@
 ---Exercicio4.hs
 --------------------------------------
 
-div10 :: Int -> Int
-div10 x = x `div` 10
-
-mod10 :: Int -> Int
-mod10 x = x `mod` 10
-
-modMulti :: Int -> Int -> Int
-modMulti x y = x * 10 + y
-
 invertInt :: Int -> Int
 invertInt x = aux x 0
 
 aux :: Int -> Int -> Int
 aux 0 acumulador = acumulador
-aux resto acumulador = aux (div10 resto) (acumulador * 10 + mod10 resto)
+aux resto acumulador = aux (resto`div`10) (acumulador * 10 + resto `mod` 10)
 
 main = do
-    print(div10 1)
-    print(mod10 1)
-    print(invertInt 123)
+    print(invertInt 12345)
