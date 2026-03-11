6b3f9d12d235f78e2761205b7cf435d412ba99a0
Carlos Eduardo <contato.devcarlos@gmail.com>
2026-03-11 11:26:06 -0300

    refatora funções de divisão e módulo, renomeando e ajustando a função main para exibir resultados

--- ListaExercicios/Lista01/Exercicio4.hs
+++ ListaExercicios/Lista01/Exercicio4.hs
@@ -2,12 +2,23 @@
 ---Exercicio4.hs
 --------------------------------------
 
-divisao :: Int -> Int
-divisao x = x `div` 10
+div10 :: Int -> Int
+div10 x = x `div` 10
 
-modulo :: Int -> Int
-modulo x = x `mod` 10
+mod10 :: Int -> Int
+mod10 x = x `mod` 10
+
+modMulti :: Int -> Int -> Int
+modMulti x y = x * 10 + y
+
+invertInt :: Int -> Int
+invertInt x = aux x 0
+
+aux :: Int -> Int -> Int
+aux 0 acumulador = acumulador
+aux resto acumulador = aux (div10 resto) (acumulador * 10 + mod10 resto)
 
 main = do
-    print(divisao 123)
-    print(modulo 123)
+    print(div10 1)
+    print(mod10 1)
+    print(invertInt 123)
