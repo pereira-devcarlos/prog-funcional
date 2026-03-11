64c87d1c8a24263ccf68f9767f157def5ca38e54
Carlos Eduardo <contato.devcarlos@gmail.com>
2026-03-11 10:19:02 -0300

    adiciona o arquivo Exercicio4.hs com a implementação da função divisao

---
+++ ListaExercicios/Lista01/Exercicio4.hs
@@ -0,0 +1,9 @@
+--------------------------------------
+---Exercicio4.hs
+--------------------------------------
+
+divisao :: Int -> Int
+divisao x = x `div` 10
+
+main = do
+    print(divisao 123)
