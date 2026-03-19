b0f1008b7a457c3717e603c823dad886379a462f
pereira-devcarlos <contato.devcarlos@gmail.com>
2026-03-19 15:39:04 -0300

    adiciona comentários descritivos para as funções e organiza a estrutura dos exercícios 8, 9 e 10

--- ListaExercicios/Lista01/Exercicio8.hs
+++ ListaExercicios/Lista01/Exercicio8.hs
@@ -1,7 +1,7 @@
 --------------------------------------
 ---Exercicio 8 
 --------------------------------------
-
+-- Máximo divisor comum (MDC) entre dois números
 mdc :: Int -> Int -> Int
 mdc m n
     | m`mod`n == 0 = n
@@ -10,9 +10,16 @@ mdc m n
 --------------------------------------
 ---Exercicio 9
 --------------------------------------
-
+-- Quantos múltiplos de um número existem em um intervalo
 howManyMultiples :: Int -> Int -> Int -> Int
 howManyMultiples num inicio fim
     | inicio > fim = 0
     | inicio `mod` num == 0 = 1 + howManyMultiples num (inicio+1) fim
     | otherwise = howManyMultiples num (inicio+1) fim
+
+--------------------------------------
+---Exercicio 10
+--------------------------------------
+-- Último dígito de um número
+lastDigit :: Int -> Int
+lastDigit x = x `mod` 10
