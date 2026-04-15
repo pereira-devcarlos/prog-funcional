b1ed9508d4e476423617f59708448dee855de8f4
Carlos Pereira <contato.devcarlos@gmail.com>
2026-04-15 00:03:18 -0300

    Adiciona funções para calcular vendas e listar vendas em um período

--- Aulas/Aula03.hs
+++ Aulas/Aula03.hs
@@ -1,3 +1,4 @@
+import System.Win32 (xBUTTON1)
 ---------------------------------------------
 ---Aula03.hs
 ---------------------------------------------
@@ -18,3 +19,29 @@ member _ [] = False
 member x (y:ys)
     | x == y = True
     | otherwise = member x ys
+
+periodo :: Int
+periodo = 7
+
+maxi :: Int -> Int -> Int
+maxi m n
+    | m >= n = m
+    | otherwise = n
+
+vendas :: Int -> Int
+vendas 1 = 24
+vendas 2 = 15
+vendas 3 = 17
+vendas 4 = 20
+vendas 5 = 22
+vendas 6 = 18
+vendas 7 = 25
+vendas _ = 0
+
+listSales :: [Int]
+listSales = listSalesAux 1
+
+listSalesAux :: Int -> [Int]
+listSalesAux x
+    | x > periodo = []
+    | otherwise = vendas x : listSalesAux(x+1)
