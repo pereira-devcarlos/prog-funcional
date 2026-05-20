9d33e494aed41e9ca52ca48166f3101f592bf86e
Carlos Pereira <contato.devcarlos@gmail.com>
2026-05-20 07:09:35 -0300

    feat: adiciona função para verificar a presença de um elemento em sublistas

--- Provas/Prova01.hs
+++ Provas/Prova01.hs
@@ -9,3 +9,13 @@ teremos um Bool que informa se a ∈ b.
 f_in :: Int -> [Int] -> Bool
 f_in _ [] = False
 f_in x (y:ys) = x == y || f_in x ys
+
+---- Questão 2
+{-
+Considerando f_in, faça, em Haskell, uma solução que receba um Int x e uma [[Int]] l.
+Como saída, teremos uma [(Bool, [Int])] s que informa, para cada sublista li de l, se o elemento
+x pertence ou não à li.
+-}
+f2 :: Int -> [[Int]] -> [(Bool, [Int])]
+f2 _ [] = []
+f2 x (y:ys) = (f_in x y, y) : f2 x ys
