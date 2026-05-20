83a370d76f3816feba71b8916d39ce793b1e13fc
Carlos Pereira <contato.devcarlos@gmail.com>
2026-05-20 07:09:51 -0300

    feat: adiciona função f3 para verificar a presença de um elemento em sublistas

--- Provas/Prova01.hs
+++ Provas/Prova01.hs
@@ -19,3 +19,12 @@ x pertence ou não à li.
 f2 :: Int -> [[Int]] -> [(Bool, [Int])]
 f2 _ [] = []
 f2 x (y:ys) = (f_in x y, y) : f2 x ys
+
+---- Questão 3
+{-
+Considerando f2, faça, em Haskell, uma solução que receba um Int x e uma [[Int]] l.
+Como saída, teremos uma (Int, [(Bool, [Int])]) s que informa o Int x e, para cada sublista li
+de l, se o elemento x pertence ou não à li.
+-}
+f3 :: Int -> [[Int]] -> (Int, [(Bool,[Int])])
+f3 x y = (x, f2 x y)
