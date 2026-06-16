1466657fa1266cc1a1f391d8156afdee7aa41514
pereira-devcarlos <contato.devcarlos@gmail.com>
2026-06-16 13:40:18 -0300

    feat: adiciona função squareOfEven para calcular o quadrado de números pares em uma lista

--- Aulas/Aula05.hs
+++ Aulas/Aula05.hs
@@ -10,3 +10,8 @@ square x = map (^2) x
 multiples5 :: [Int] -> [Int]
 multiples5 x =  filter xs x
     where xs a = a `mod` 5 == 0
+
+-- Exercise 3
+squareOfEven :: [Int] -> [Int]
+squareOfEven x = map (^2) (filter xs x)
+    where xs a = a `mod` 2 == 0
