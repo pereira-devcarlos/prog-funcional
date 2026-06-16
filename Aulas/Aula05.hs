a0f93531d6c3e2c4ddeeeb5e239c939ca361db2f
pereira-devcarlos <contato.devcarlos@gmail.com>
2026-06-16 13:40:07 -0300

    fix: corrige erros de digitação nos comentários das funções de exercício

--- Aulas/Aula05.hs
+++ Aulas/Aula05.hs
@@ -2,11 +2,11 @@
 ---Aula03.hs
 ---------------------------------------------
 
--- Exercice 1
+-- Exercise 1
 square :: [Int] -> [Int]
 square x = map (^2) x
 
--- Exercice 2
+-- Exercise 2
 multiples5 :: [Int] -> [Int]
 multiples5 x =  filter xs x
     where xs a = a `mod` 5 == 0
