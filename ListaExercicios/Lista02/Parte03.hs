5ecf9fc4c664542af8f0046a93820aa80f63a8e6
Carlos Pereira <contato.devcarlos@gmail.com>
2026-06-24 09:59:32 -0300

    feat: adiciona exercícios 12 a 15 de manipulação de listas

--- ListaExercicios/Lista02/Parte03.hs
+++ ListaExercicios/Lista02/Parte03.hs
@@ -3,6 +3,25 @@
 --------------------------------------
 
 --------------------------------------
----Exercício 11
+---Exercício 12
 --------------------------------------
--- A fazer
+evenCubes :: Int -> [Int]
+evenCubes x = map (^3) (filter even [1..(x-1)])
+
+--------------------------------------
+---Exercício 13
+--------------------------------------
+insertOrd :: Int -> [Int] -> [Int]
+insertOrd x xs = [a | a<- xs, a < x] ++ [x] ++ [b | b<-xs, b>=x]
+
+--------------------------------------
+---Exercício 14
+--------------------------------------
+howManyMultiples :: Int -> Int -> Int -> Int
+howManyMultiples x a b = sum [1 | y<-[a..b], y`mod`x == 0]
+
+--------------------------------------
+---Exercício 15
+--------------------------------------
+duplicate :: String -> Int -> String
+duplicate x y = concat [x | _<-[1..y]]
